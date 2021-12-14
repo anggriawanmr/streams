5058700b2ab27d109f9e3d6df23b925e53859a28
anggriawanmr <anggriawanmr@gmail.com>
2021-12-14 15:47:25 +0700

    Adding validation :sparkles:

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -11,15 +11,32 @@ class StreamCreate extends React.Component {
     );
   }
 
+  onSubmit(formValues) {}
+
   render() {
     return (
-      <form className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Submmit</button>
       </form>
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
 
 export default reduxForm({
   form: "StreamCreate",
